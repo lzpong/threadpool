8fedb5343309854f6017ea769ace93958805570a
lzpong <lzpong@163.com>
2017-05-15 17:47:56 +0800

    Fix. add thr

--- threadpool.h
+++ threadpool.h
@@ -13,36 +13,28 @@
 
 namespace std
 {
-#define  THREADPOOL_MAX_NUM 128
+//线程池最大容量,应尽量设小一点
+#define  THREADPOOL_MAX_NUM 64
 
 //线程池,可以提交变参函数或拉姆达表达式的匿名函数执行,可以获取执行返回值
 //不直接支持类成员函数, 支持类静态成员函数或全局函数,Opteron()函数等
 class threadpool
 {
-	using Task = std::function<void()>;
-	// 线程池
-	std::vector<std::thread> pool;
-	// 任务队列
-	std::queue<Task> tasks;
-	// 同步
-	std::mutex m_lock;
-	// 条件阻塞
-	std::condition_variable cv_task;
-	// 是否关闭提交
-	std::atomic<bool> stoped;
-	//空闲线程数量
-	std::atomic<int>  idlThrNum;
+	using Task = std::function<void()>;	//定义类型
+	std::vector<std::thread> _pool;     //线程池
+	std::queue<Task> _tasks;            //任务队列
+	std::mutex _lock;                   //同步
+	std::condition_variable _task_cv;   //条件阻塞
+	std::atomic<bool> _run;             //线程池是否执行
+	std::atomic<int>  _idlThrNum;       //空闲线程数量
 
 public:
-	inline threadpool(unsigned short size = 4) :idlThrNum{ 0 }, stoped{ false }
-	{
-		addThr(size);
-	}
+	inline threadpool(unsigned short size = 4) :_idlThrNum{ 0 }, _run{ true } { addThread(size); }
 	inline ~threadpool()
 	{
-		stoped.store(true);
-		cv_task.notify_all(); // 唤醒所有线程执行
-		for (std::thread& thread : pool) {
+		_run=false;
+		_task_cv.notify_all(); // 唤醒所有线程执行
+		for (std::thread& thread : _pool) {
 			//thread.detach(); // 让线程“自生自灭”
 			if(thread.joinable())
 				thread.join(); // 等待任务结束， 前提：线程一定会执行完
@@ -54,71 +46,70 @@ public:
 	// 调用.get()获取返回值会等待任务执行完,获取返回值
 	// 有两种方法可以实现调用类成员，
 	// 一种是使用   bind： .commit(std::bind(&Dog::sayHello, &dog));
-	// 一种是用 mem_fn： .commit(std::mem_fn(&Dog::sayHello), &dog)
+	// 一种是用   mem_fn： .commit(std::mem_fn(&Dog::sayHello), &dog)
 	template<class F, class... Args>
 	auto commit(F&& f, Args&&... args) ->std::future<decltype(f(args...))>
 	{
-		if (stoped.load())    // stop == true ??
+		if (!_run)    // stoped ??
 			throw std::runtime_error("commit on ThreadPool is stopped.");
 
 		using RetType = decltype(f(args...)); // typename std::result_of<F(Args...)>::type, 函数 f 的返回值类型
 		auto task = std::make_shared<std::packaged_task<RetType()> >(
 			std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-			);    // wtf !
+		);    // wtf !
 		std::future<RetType> future = task->get_future();
 		{    // 添加任务到队列
-			std::lock_guard<std::mutex> lock{ m_lock };//对当前块的语句加锁  lock_guard 是 mutex 的 stack 封装类，构造的时候 lock()，析构的时候 unlock()
-			tasks.emplace(
+			std::lock_guard<std::mutex> lock{ _lock };//对当前块的语句加锁  lock_guard 是 mutex 的 stack 封装类，构造的时候 lock()，析构的时候 unlock()
+			_tasks.emplace(
 				[task]()
-				{ // push(Task{...})
+				{ // push(Task{...}) 放到队列后面
 					(*task)();
 				}
 			);
 		}
-		if (idlThrNum < 1 && idlThrNum < THREADPOOL_MAX_NUM)
-			addThr(1);
-		cv_task.notify_one(); // 唤醒一个线程执行
+		if (_idlThrNum < 1 && _pool.size() < THREADPOOL_MAX_NUM)
+			addThread(1);
+		_task_cv.notify_one(); // 唤醒一个线程执行
 
 		return future;
 	}
 
 	//空闲线程数量
-	int idlCount() { return idlThrNum; }
+	int idlCount() { return _idlThrNum; }
+	//线程数量
+	int thrCount() { return _pool.size(); }
 
 	//添加指定数量的线程
-	void addThr(unsigned short size)
+	void addThread(unsigned short size)
 	{
-		if (idlThrNum + size > THREADPOOL_MAX_NUM)
-		{   //限制池中线程数量
-			size = THREADPOOL_MAX_NUM - idlThrNum;
-			idlThrNum = THREADPOOL_MAX_NUM;
-		}
-		for (; size > 0; --size)
-		{   //初始化线程数量
-			pool.emplace_back(
+		for (; _pool.size() < THREADPOOL_MAX_NUM && size > 0; --size)
+		{   //增加线程数量,但不超过 预定义数量 THREADPOOL_MAX_NUM
+			_pool.emplace_back(
 				[this]
-			{ // 工作线程函数
-				for (;;)
+			{ //工作线程函数
+				while (_run)
 				{
-					std::function<void()> task;
-					{   // 获取一个待执行的 task
-						std::unique_lock<std::mutex> lock{ this->m_lock };// unique_lock 相比 lock_guard 的好处是：可以随时 unlock() 和 lock()
-						this->cv_task.wait(lock,
+					std::function<void()> task; // 获取一个待执行的 task
+					{
+						// unique_lock 相比 lock_guard 的好处是：可以随时 unlock() 和 lock()
+						std::unique_lock<std::mutex> lock{ _lock };
+						_task_cv.wait(lock,
 							[this] {
-							return this->stoped.load() || !this->tasks.empty();
-						}
-						); // wait 直到有 task
-						if (this->stoped && this->tasks.empty())
+								return !_run || !_tasks.empty();
+							} // wait 直到有 task
+						);
+						if (!_run && _tasks.empty())
 							return;
-						task = std::move(this->tasks.front()); // 取一个 task
-						this->tasks.pop();
+						task = std::move(_tasks.front()); // 按先进先出从队列取一个 task
+						_tasks.pop();
 					}
-					idlThrNum--;
-					task();
-					idlThrNum++;
+					_idlThrNum--;
+					task();//执行任务
+					_idlThrNum++;
 				}
 			}
 			);
+			_idlThrNum++;
 		}
 
 	}
@@ -126,4 +117,4 @@ public:
 
 }
 
-#endif
+#endif  //https://github.com/lzpong/
